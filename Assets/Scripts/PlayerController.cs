1a98bb322face874461e702fdbab8814229d1e20
K-Wind <kasperwind>
2017-08-25 16:36:32 +0200

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public Vector3 Direction;
+
+	// Use this for initialization
+	void Start () {
+        		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!isLocalPlayer)
+	    {
+	        return;
+	    }
+
+        var rotation = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+	    var movement = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+	    transform.Rotate(0, 0, -rotation);
+	    transform.Translate(0, movement, 0);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        foreach (MeshRenderer mesh in GetComponentsInChildren<MeshRenderer>())
+        {
+            mesh.material.color = Color.blue;
+        }
+    }
+}
---
+++ Assets/Scripts/Radar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+
+    public float timePerScan = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Scan", 0, timePerScan);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void Scan()
+    {
+        Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, 100);
+        Vector3 characterToCollider;
+        float dot;
+        foreach (Collider2D collider in cols)
+        {
+            characterToCollider = (collider.transform.position - transform.position).normalized;
+            dot = Vector3.Dot(characterToCollider, transform.forward);
+            if (dot >= Mathf.Cos(55))
+            {
+                if (collider.tag == "Enemy")
+                {
+                    collider.gameObject.GetComponent<MeshRenderer>().enabled = true;
+                }
+            }
+            else
+            {
+                //objectSpotted = false;
+            }
+        }
+    }
+}
